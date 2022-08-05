7797f08eb5079b90ab455c4a7200dd7f349a93f4
AdrianaBerdich <adriana_berdich@yahoo.com>
2022-08-05 19:59:17 +0300

    Add files via upload

---
+++ attack_setUp.m
@@ -0,0 +1,24 @@
+probability = 2^(-4);
+seed_probability = 1;
+
+max_delay = 1;                       %s
+
+fuzzy_min_vs = 0;%-255/3.6;%0;                    %m/s
+fuzzy_max_vs = 255/3.6;              %m/s
+
+fuzzy_min_acel = -3;%3;%3;%12.8;          %m/s^2
+fuzzy_max_acel = 3;%;%3;%12.7;          %m/s^2
+
+fuzzy_min_decel = -12.8;          %m/s^2
+fuzzy_max_decel = 12.7;          %m/s^2
+
+fuzzy_min_dist = 0;%-200;      %m
+fuzzy_max_dist = 255;%200;%0;   %m
+
+fuzzy_min_curvature = 0;
+fuzzy_max_curvature = 1;
+
+fuzzy_min_rel_vs = -255/3.6;%0;                    %m/s
+fuzzy_max_rel_vs = 0;%255/3.6;              %m/s
+
+
